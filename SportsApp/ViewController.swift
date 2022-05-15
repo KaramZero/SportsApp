131c1e8d2fe1c05aca484e46a18cfc7d0a13fd9e
Karam Ibrahim <94894846+KaramZero@users.noreply.github.com>
2022-05-15 14:08:06 -0700

    Initial Commit

---
+++ SportsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by Karam Ibrahim on 5/15/22.
+//  Copyright © 2022 Karam Ibrahim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SportsAppTests/SportsAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  SportsAppTests.swift
+//  SportsAppTests
+//
+//  Created by Karam Ibrahim on 5/15/22.
+//  Copyright © 2022 Karam Ibrahim. All rights reserved.
+//
+
+import XCTest
+@testable import SportsApp
+
+class SportsAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
